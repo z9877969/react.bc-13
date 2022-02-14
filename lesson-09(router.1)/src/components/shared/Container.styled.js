0aad0fcee40cd8d3ab49bdea85984b4ed36847e3
z9877969 <z9877969@ukr.net>
2022-02-14 17:00:26 +0200

    removes templates, adds routes schema

--- lesson-09(router.1)/src/components/shared/Container.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const ContainerStyled = styled.div`
-  width: 1000px;
-  padding: 0 15px;
-  background-color: tomato;
-`;
--- lesson-09(router.1)/src/components/shared/MainContainer.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const MainContainerStyled = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 0 auto;
-`;
